ad72afa653895306648ac3532d3657bc82fbbcc5
andres <sahun@archilyse.com>
2018-09-24 17:07:59 +0200

    server file

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
+const uaRedirect = require('express-ua-redirect');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/arch-site'));
+app.use(express.static(__dirname + '/dist'));
 
-console.log(process.env.NODE_ENV);
+app.use(uaRedirect({
+  browsers: {
+    authorized: {
+      safari: '7+',
+      IE: '9+'
+    },
+    evergreen: true
+  },
+  redirectTo: '/upgrade'
+}));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.get('x-forwarded-proto') !== 'https') {
-      res.redirect(`https://${req.header('host')}${req.url}`);
-    } else {
-      next();
-    }
-  });
-}
-
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/arch-site/index.html');
+app.get('/*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, function() {
-  console.log('App is running on port ' + port);
+app.listen(port, function () {
+  console.log("App is running on port " + port);
 });
